6e0ee5abe234bfe69277c86ddf17fc15c37d394c
xinke.wang <243090886@qq.com>
2025-11-17 11:52:07 +0800

    初始化剩余文件提交

--- src/main/java/com/suke/czx/Application.java
+++ src/main/java/com/suke/czx/Application.java
@@ -1,13 +1,19 @@
 package com.suke.czx;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @EnableScheduling
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.suke.wxk.repository")
+@MapperScan("com.suke.wxk.mapper")
+@ComponentScan(basePackages = {"com.suke.czx", "com.suke.wxk"})
 public class Application {
 
     public static void main(String[] args) {
